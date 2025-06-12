1e25405487cae2c0396f6d835fc69aa5723dac6c
edwin <edwin@gmail.com>
2025-06-12 14:55:58 -0500

    correcion

--- senalink/models/Programa.php
+++ senalink/models/Programa.php
@@ -11,9 +11,9 @@ class ProgramaFormacion {
     public function crear($data) {
         $sql = "INSERT INTO programas_formacion 
                 (id,codigo, ficha, nivel_formacion, nombre_programa, 
-                 duracion_meses, estado, descripcion, habilidades_requeridas, fecha_finalizacion)
-                VALUES (:codigo, :ficha, :nivel_formacion, :nombre_programa,
-                 :duracion_meses, :estado, :descripcion, :habilidades_requeridas, :fecha_finalizacion)";
+                duracion_meses, estado, descripcion, habilidades_requeridas, fecha_finalizacion)
+                VALUES (:id,:codigo, :ficha, :nivel_formacion, :nombre_programa,
+                :duracion_meses, :estado, :descripcion, :habilidades_requeridas, :fecha_finalizacion)";
 
         $stmt = $this->db->prepare($sql);
 
