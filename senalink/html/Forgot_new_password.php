992ea41a7f7f664aea3bbd615ded92f8cdaed4d2
pele <pele@gmail.com>
2025-06-19 18:52:12 -0500

    avance correcion forgot password

--- senalink/html/Forgot_new_password.php
+++ senalink/html/Forgot_new_password.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8"/>
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
     <title>SenaLink - Nueva Contraseña</title>
-    <link href="../css/forgot_new_password.css" rel="stylesheet"/>
+    <link href="../css/Inputs.css" rel="stylesheet"/>
     <link rel="shortcut icon " href="../img/Favicon1.png"> <!-- corresponde al favicon en la pestaña -->
 </head>
 <body>
@@ -17,22 +17,35 @@
             </a>
         </div>
     </header>
-  <header class="header__home">
-    <button id="menu-toggle" class="menu-toggle">
-      <img src="../icon/menu_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Menu Icon" />
-    </button>
-    <button class="account__button">
-      <a href="../viewuser.html">
-        <img src="../icon/account_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="icon" />
-      </a>
-    </button>
-</header>
+    <header class="header__login">
+        <div class="header__left">
+            
+        </div>
+
+        <div class="header__center">
+        </div>
+
+        <div class="header__right">
+            <img src="../img/logo-sena-white0.png" alt="Logo Izquierda" class="logo__sena">
+        </div>
+    </header>
+    <div class="linea-verde"></div>
 <div class="container">
   <main class="container__forms--forgot">
         <h2>Ingresa tu nueva contraseña</h2>
         <form action="../controllers/update_password.php" method="POST" id="newPasswordForm">
-          <input type="password" name="new_password" placeholder="Nueva contraseña" required/>
-          <input type="password" name="confirm_password" placeholder="Confirmar nueva contraseña" required/>
+          <div class="box__password">
+            <input type="password" name="new_password" placeholder="Nueva contraseña" required/>
+            <button type="button" class="toggle-password-button">
+              <img src="../img/icons8-invisible-24.png" alt="Mostrar/Ocultar contraseña" class="icon__pass" />
+            </button>
+          </div>
+          <div class="box__password">
+            <input type="password" name="confirm_password" placeholder="Confirmar nueva contraseña" required/>
+            <button type="button" class="toggle-password-button">
+              <img src="../img/icons8-invisible-24.png" alt="Mostrar/Ocultar contraseña" class="icon__pass" />
+            </button>
+          </div>
           <input type="hidden" name="token" value="<?php echo htmlspecialchars($_SESSION['token_recuperacion'] ?? '') ?>" />
           <div class="buttons__container">
               <button type="submit" class="btn">Confirmar</button>
@@ -45,5 +58,24 @@
   <footer>
     <p>@ Todos los derechos reservados. SenaLink</p>
   </footer>
+  <script src="../js/viewpassword.js"></script>
+  <script>
+    document.addEventListener("DOMContentLoaded", function () {
+      function setupTogglePassword(inputName, buttonClass) {
+        const input = document.querySelector('input[name="' + inputName + '"]');
+        const toggleButtons = document.querySelectorAll('.' + buttonClass);
+        toggleButtons.forEach(function(button) {
+          button.addEventListener('click', function() {
+            const icon = button.querySelector('img');
+            const isPassword = input.type === "password";
+            input.type = isPassword ? "text" : "password";
+            icon.src = isPassword ? "../img/icons8-eye-24.png" : "../img/icons8-invisible-24.png";
+          });
+        });
+      }
+      setupTogglePassword('new_password', 'toggle-password-button');
+      setupTogglePassword('confirm_password', 'toggle-password-button');
+    });
+  </script>
 </body>
 </html>
