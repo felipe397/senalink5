f2b6b413ed6d3a838c76d4b26910d790d11ba0db
pele <pele@gmail.com>
2025-06-19 17:26:57 -0500

    conexion puertos avance

---
+++ senalink/Config/config_local.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'host' => 'localhost',     // No cambies esto
+    'port' => '3307',          // Cambia esto según tu puerto (por ejemplo: 3306, 3308)
+    'db'   => 'senalink',
+    'user' => 'root',
+    'pass' => '',              // Pon tu contraseña si usas una
+];
