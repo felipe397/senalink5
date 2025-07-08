07f783f582e8651ced4d73da010f86131ddd66b9
ruth <ruth@gmail.com>
2025-07-08 15:11:44 -0500

    .

--- senalink/Config/conexion.php
+++ senalink/Config/conexion.php
@@ -4,9 +4,9 @@ class Conexion {
 
     public static function conectar() {
         if (self::$conn === null) {
-            $host = 'localhost'; // Cambia esto si tu servidor MySQL está en otro host
+            // $host = 'localhost'; // Cambia esto si tu servidor MySQL está en otro host
             $host = 'localhost:3307'; 
-            $host = 'localhost:8111'; 
+            // $host = 'localhost:8111'; 
             $db = 'senalink';
             $user = 'root';
             $pass = ''; // o tu contraseña si la tienes
